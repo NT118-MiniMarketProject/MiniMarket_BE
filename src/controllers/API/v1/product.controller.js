e9c445962569679cc3109b4b96ad3164f1de5b7a
ThanhLinh023 <vuongthanhlinh2003.contact@gmail.com>
2024-04-25 10:42:33 +0700

    add_remove_wishlist

---
+++ src/controllers/API/v1/product.controller.js
@@ -0,0 +1,31 @@
+const {StatusCodes} = require('http-status-codes')
+const ProductService = require('../../../services/API/v1/product.service')
+
+const addWishList = async (req, res, next) => {
+    try {
+        const body = req.body
+
+        const {list} = await ProductService.addToWishList({body, req, res})
+        res.status(StatusCodes.OK).json({Wishlist: list})
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+const removeWishList = async (req, res, next) => {
+    try {
+        const body = req.body
+
+        const {list} = await ProductService.removeFromWishList({body, req, res})
+        res.status(StatusCodes.OK).json({Wishlist: list})
+    }
+    catch (err) {
+        next(err)
+    }
+}
+
+module.exports = {
+    addWishList,
+    removeWishList
+}
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
+const {addWishList, removeWishList} = require('../controllers/API/v1/product.controller')
+const  {
+    authenticateUser
+} = require('../middleware/authentication')
 
+router.post('/wishlist/add', authenticateUser, addWishList)
+router.post('/wishlist/remove', authenticateUser, removeWishList)
 module.exports = router
