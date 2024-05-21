1d00a7e00b6fa94cc20796bc17deb6b0d98513a4
NgTrGKiet <kierngr@gmail.com>
2024-05-21 13:35:35 +0700

    config redis, them cac file chuan bi tao lap don hang

---
+++ src/controllers/order.controller.js
@@ -0,0 +1,33 @@
+const {StatusCodes} = require('http-status-codes')
+const CustomError = require('../errors')
+const OrderService = require('../services/order.service')
+
+const CreateOrder = async(req, res, next) => {
+    try {
+
+    } catch (err) {
+
+    }
+}
+
+const OrderList = async(req, res, next) => {
+    try {
+
+    } catch (err) {
+
+    }
+}
+
+const DetailOrder = async(req, res, next) => {
+    try {
+
+    } catch (err) {
+
+    }
+}
+
+module.exports = {
+    CreateOrder,
+    OrderList,
+    DetailOrder
+}
