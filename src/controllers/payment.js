ee94ddedc921bd28aec2e2dc40755aa1eea3a493
NgTrGKiet <kierngr@gmail.com>
2024-05-28 09:24:05 +0700

    vnpay va gui mail khi mua hang thanh cong

--- src/controllers/payment.js
+++ src/controllers/payment.js
@@ -1,80 +1,103 @@
-const checkoutVNPay = async(req, res, next) => {
-  console.log(req.query);
-  const totalPrice = req.query.totalPrice;
+const moment = require('moment')
 
-  process.env.TZ = "Asia/Ho_Chi_Minh";
-  let dateFormat = require("dateformat");
-  let date = new Date();
+
+const checkOutVNPay = async (req, res, next) => {
+  process.env.TZ = 'Asia/Ho_Chi_Minh'
+
+  //Order
+  const { name, email, products, address, amount } = req.body
+  // console.log({ name, email, products, address, amount } )
+  // console.log(amount)
+  // let dump_products = [
+  //   { productName: 'Sản phẩm 1', price: 50.0 },
+  //   { productName: 'Sản phẩm 2', price: 30.0 },
+  // ]
+  //Checkout
+
+  let date = new Date()
+  let createDate = moment(date).format('YYYYMMDDHHmmss')
 
   let ipAddr =
-    req.headers["x-forwarded-for"] ||
+    req.headers['x-forwarded-for'] ||
     req.connection.remoteAddress ||
     req.socket.remoteAddress ||
-    req.connection.socket.remoteAddress;
+    req.connection.socket.remoteAddress
 
-  let config = require("config");
+  let config = require('../config/vn-pay.json')
 
-  let tmnCode = config.get("vnp_TmnCode");
-  let secretKey = config.get("vnp_HashSecret");
-  let vnpUrl = config.get("vnp_Url");
-  let returnUrl = config.get("vnp_ReturnUrl");
-  let createDate = dateFormat(date, "yyyymmddHHmmss");
-  let orderId = dateFormat(date, "HHmmss");
-  let amount = +totalPrice;
-  let bankCode = "";
+  let tmnCode = config.vnp_TmnCode
+  let secretKey = config.vnp_HashSecret
+  let vnpUrl = config.vnp_Url
+  let returnUrl = config.vnp_ReturnUrl
+  let orderId = moment(date).format('DDHHmmss')
+  let bankCode = 'NCB'
 
-  let locale = "vn";
-  if (locale === null || locale === "") {
-    locale = "vn";
-  }
+  let locale = 'vn'
+  // if (locale === null || locale === '') {
+  //   locale = 'vn'
+  // }
+  let currCode = 'VND'
+  let vnp_Params = {}
+  vnp_Params['vnp_Version'] = '2.1.0'
+  vnp_Params['vnp_Command'] = 'pay'
+  vnp_Params['vnp_TmnCode'] = tmnCode
+  vnp_Params['vnp_Locale'] = locale
+  vnp_Params['vnp_CurrCode'] = currCode
+  vnp_Params['vnp_TxnRef'] = orderId
+  vnp_Params['vnp_OrderInfo'] = 'Thanh toan cho ma GD:' + orderId
+  vnp_Params['vnp_OrderType'] = 'other'
+  vnp_Params['vnp_Amount'] = amount * 100
+  vnp_Params['vnp_ReturnUrl'] = returnUrl
+  vnp_Params['vnp_IpAddr'] = ipAddr
+  vnp_Params['vnp_CreateDate'] = createDate
 
-  let currCode = "VND";
-  let vnp_Params = {};
-  vnp_Params["vnp_Version"] = "2.1.0";
-  vnp_Params["vnp_Command"] = "pay";
-  vnp_Params["vnp_TmnCode"] = tmnCode;
-  vnp_Params["vnp_Locale"] = locale;
-  vnp_Params["vnp_CurrCode"] = currCode;
-  vnp_Params["vnp_TxnRef"] = orderId;
-  vnp_Params["vnp_OrderInfo"] = "Thanh toan cho ma GD:" + orderId;
-  vnp_Params["vnp_OrderType"] = "billpayment";
-  vnp_Params["vnp_Amount"] = amount * 100;
-  vnp_Params["vnp_ReturnUrl"] = returnUrl;
-  vnp_Params["vnp_IpAddr"] = ipAddr;
-  vnp_Params["vnp_CreateDate"] = createDate;
-  if (bankCode !== null && bankCode !== "") {
-    vnp_Params["vnp_BankCode"] = bankCode;
-  }
+  vnp_Params = sortObject(vnp_Params)
 
-  vnp_Params = sortObject(vnp_Params);
-
-  let querystring = require("qs");
-  let signData = querystring.stringify(vnp_Params, { encode: false });
-  let crypto = require("crypto");
-  let hmac = crypto.createHmac("sha512", secretKey);
-  let signed = hmac.update(Buffer.from(signData, "utf-8")).digest("hex");
-  vnp_Params["vnp_SecureHash"] = signed;
-  vnpUrl += "?" + querystring.stringify(vnp_Params, { encode: false });
-
-  res.redirect(vnpUrl);
+  let querystring = require('qs')
+  let signData = querystring.stringify(vnp_Params, { encode: false })
+  let crypto = require('crypto')
+  let hmac = crypto.createHmac('sha512', secretKey)
+  let signed = hmac.update(new Buffer(signData, 'utf-8')).digest('hex')
+  vnp_Params['vnp_SecureHash'] = signed
+  vnpUrl += '?' + querystring.stringify(vnp_Params, { encode: false })
+  emailDetail.name = name
+  emailDetail.email = email
+  emailDetail.products = products //dump_products
+  emailDetail.address = address
+  emailDetail.amount = amount
+  // console.log(vnp_Params)
+  // res.redirect(vnpUrl);
+  return res.json({ vnpUrl })
 }
 
 function sortObject(obj) {
-  let sorted = {};
-  let str = [];
-  let key;
+  let sorted = {}
+  let str = []
+  let key
   for (key in obj) {
     if (obj.hasOwnProperty(key)) {
-      str.push(encodeURIComponent(key));
+      str.push(encodeURIComponent(key))
     }
   }
-  str.sort();
+  str.sort()
   for (key = 0; key < str.length; key++) {
-    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, "+");
+    sorted[str[key]] = encodeURIComponent(obj[str[key]]).replace(/%20/g, '+')
   }
-  return sorted;
+  return sorted
+}
+
+const vnpay_return = async (req, res, next) => {
+  let vnp_Params = req.query
+  try {
+    const mail = await service.sendEmail(emailDetail)
+  } catch (err) {
+    // console.log(err)
+    return err
+  }
+  return res.json({ code: vnp_Params['vnp_ResponseCode'] })
 }
 
 module.exports = {
-    checkoutVNPay
+  checkOutVNPay,
+  vnpay_return,
 }
